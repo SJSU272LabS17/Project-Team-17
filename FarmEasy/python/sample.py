b0cc916b317664f11ccd3b8904a680ed7f461119
noopurjoshi <noopur2393@gmail.com>
2017-05-15 20:31:19 -0700

    Changes for adding new counties and sales data.

--- FarmEasy/python/sample.py
+++ FarmEasy/python/sample.py
@@ -26,19 +26,23 @@ class sample(object):
         acres_harv_irr_str = cropName + ", IRRIGATED - ACRES HARVESTED"
         production_irr_str = cropName + ", IRRIGATED - PRODUCTION, MEASURED IN BU"
         yield_irr_str = cropName + ", IRRIGATED - YIELD, MEASURED IN BU / ACRE"
+        sales_irr_str = cropName + ", IRRIGATED - SALES, MEASURED IN $"
         acres_harvested_irr = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & (temp.data_item == acres_plant_irr_str)]
         acres_planted_irr = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & (temp.data_item == acres_harv_irr_str)]
         production_irr = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & (temp.data_item == production_irr_str)]
         yield_measured_irr = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & (temp.data_item == yield_irr_str)]
-         
+        sales_measured_irr = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & (temp.data_item == sales_irr_str)]
+
         acres_plant_nirr_str = cropName + ", NON-IRRIGATED - ACRES PLANTED"
         acres_harv_nirr_str = cropName + ", NON-IRRIGATED - ACRES HARVESTED"
         production_nirr_str = cropName + ", NON-IRRIGATED - PRODUCTION, MEASURED IN BU"
         yield_nirr_str = cropName + ", NON-IRRIGATED - YIELD, MEASURED IN BU / ACRE"
+        sales_nirr_str = cropName + ", NON-IRRIGATED - SALES, MEASURED IN $"
         acres_harvested_nirr = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & (temp.data_item == acres_plant_nirr_str)]
         acres_planted_nirr = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & (temp.data_item == acres_harv_nirr_str)]
         production_nirr = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & (temp.data_item == production_nirr_str)]
         yield_measured_nirr = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & (temp.data_item == yield_nirr_str)]
+        sales_measured_nirr = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & (temp.data_item == sales_nirr_str)]
         list = {}
         
         list.update({'acres_harvested': acres_harvested.value.mean().round(2)})
@@ -50,10 +54,12 @@ class sample(object):
         list.update({'acres_planted_irr': acres_planted_irr.value.mean().round(2)})
         list.update({'production_irr': production_irr.value.mean().round(2)})
         list.update({'yield_irr': yield_measured_irr.value.mean().round(2)})
+        list.update({'sales_irr': sales_measured_irr.value.mean().round(2)})
         list.update({'acres_harvested_nirr': acres_harvested_nirr.value.mean().round(2)})
         list.update({'acres_planted_nirr': acres_planted_nirr.value.mean().round(2)})
         list.update({'production_nirr': production_nirr.value.mean().round(2)})
         list.update({'yield_nirr': yield_measured_nirr.value.mean().round(2)})
+        list.update({'sales_nirr': sales_measured_nirr.value.mean().round(2)})
 
         a = self.cli.prepare(list, cropName)
         return a
